8af02202e84082d8db57ef5b6d1df66526bf101e
jordan <jchoi@ucvts.org>
2019-12-12 13:24:38 -0500

    changed window color and print text

--- text.py
+++ text.py
@@ -4,6 +4,16 @@ import pygame
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
 
+#make colors
+red = pygame.Color(255, 0, 0)
+teal = pygame.Color(0, 204, 204)
+bday = pygame.Color(90, 30, 30)
+
+# create font object
+font = pygame.font.SysFont("Garamond", 50)
+# create text
+text = font.render("Jordan", True, teal)
+
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 300) )
 
@@ -16,3 +26,7 @@ while True:
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
+    screen.fill(0, 0)
+    screen.blit(text, pygame.mouse.get_pos())
+    #fill screen with color
+    pygame.display.flip()
